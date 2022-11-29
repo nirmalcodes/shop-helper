a77eec3bbe4d5d5e7ed549704b50c11a57fa74ad
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2022-11-29 20:41:29 +0530

    Install packages

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+const Home = () => {
+  return (
+    <Container>
+      <Row>
+        <Col lg={6}>picture</Col>
+        <Col lg={6}>form</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Home;
