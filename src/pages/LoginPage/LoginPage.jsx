a032794a23c3076718697c2a50427f00be91fb74
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-03-15 00:34:35 +0530

    Update login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,95 +1,108 @@
 import React, { useState } from 'react';
 import { BsEye, BsEyeSlash } from 'react-icons/bs';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const LoginPage = () => {
     const [passwordToggled, setPasswordToggled] = useState(false);
 
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            password: '',
-        },
-        onSubmit: (values) => {
-            console.log('onSubmit', values);
-        },
-        validationSchema: Yup.object({
-            username: Yup.string().required('Username is required'),
-            password: Yup.string().required('Password is required'),
-        }),
+    const initialValues = {
+        username: '',
+        password: '',
+    };
+
+    const validationSchema = Yup.object({
+        username: Yup.string().required('Username is required'),
+        password: Yup.string().required('Password is required'),
     });
+
+    const onSubmit = (values) => {
+        console.log('onSubmit', values);
+    };
 
     return (
         <>
             <div className="mx-auto grid min-h-screen place-items-center px-4 xl:w-[70%]">
-                <div className="w-full max-w-[320px] rounded-lg bg-teal-200/20 px-4 py-8">
-                    <form onSubmit={formik.handleSubmit}>
-                        <div className="mb-3">
-                            <label
-                                htmlFor="username"
-                                className="mb-1 block text-lg"
-                            >
-                                Username
-                            </label>
-                            <input
-                                type="text"
-                                name="username"
-                                placeholder="jhone"
-                                value={formik.values.username}
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                className="block w-full rounded-md border-gray-300 shadow-sm focus:ring-blue-500"
-                            />
-                            <div className="text-sm text-red-600">
-                                {formik.errors.username &&
-                                    formik.touched.username &&
-                                    formik.errors.username}
-                            </div>
-                        </div>
-                        <div className="mb-3">
-                            <label
-                                htmlFor="password"
-                                className="mb-1 block text-lg"
-                            >
-                                Password
-                            </label>
-                            <div className="relative">
-                                <input
-                                    type={passwordToggled ? 'text' : 'password'}
-                                    name="password"
-                                    placeholder="********"
-                                    value={formik.values.password}
-                                    onChange={formik.handleChange}
-                                    onBlur={formik.handleBlur}
-                                    className="block w-full rounded-md"
-                                />
-                                <div
-                                    className="absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer rounded-full p-1 transition-all duration-150 hover:bg-slate-400/40"
-                                    onClick={() =>
-                                        setPasswordToggled(!passwordToggled)
-                                    }
-                                >
-                                    {passwordToggled ? (
-                                        <BsEyeSlash className="" />
-                                    ) : (
-                                        <BsEye className="" />
-                                    )}
+                <div className="w-full max-w-[320px] rounded-lg bg-white px-4 py-8 drop-shadow-md">
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={validationSchema}
+                        onSubmit={onSubmit}
+                    >
+                        {() => (
+                            <Form>
+                                <h4 className="mb-4 text-center text-2xl font-medium text-gray-700">
+                                    Log in
+                                </h4>
+                                <div className="form-group">
+                                    <label
+                                        htmlFor="username"
+                                        className="form-label"
+                                    >
+                                        Username
+                                    </label>
+                                    <Field
+                                        id="username"
+                                        name="username"
+                                        type="text"
+                                        placeholder="jone"
+                                        className="form-input"
+                                    />
+                                    <ErrorMessage
+                                        name="username"
+                                        component="span"
+                                        className="form-error-message"
+                                    />
                                 </div>
-                            </div>
-                            <div className="text-sm text-red-600">
-                                {formik.errors.password &&
-                                    formik.touched.password &&
-                                    formik.errors.password}
-                            </div>
-                        </div>
-                        <button
-                            type="submit"
-                            className="block w-full border-gray-300 shadow-sm focus:ring-blue-500"
-                        >
-                            sign in
-                        </button>
-                    </form>
+                                <div className="form-group">
+                                    <label
+                                        htmlFor="password"
+                                        className="form-label"
+                                    >
+                                        Password
+                                    </label>
+                                    <div className="form-password-group">
+                                        <Field
+                                            id="password"
+                                            name="password"
+                                            type={
+                                                passwordToggled
+                                                    ? 'text'
+                                                    : 'password'
+                                            }
+                                            placeholder="******"
+                                            className="form-input"
+                                        />
+                                        <div
+                                            className="password-toggler"
+                                            onClick={() =>
+                                                setPasswordToggled(
+                                                    !passwordToggled
+                                                )
+                                            }
+                                        >
+                                            {passwordToggled ? (
+                                                <BsEyeSlash className="text-gray-700" />
+                                            ) : (
+                                                <BsEye className="text-gray-700" />
+                                            )}
+                                        </div>
+                                    </div>
+                                    <ErrorMessage
+                                        name="password"
+                                        component="span"
+                                        className="form-error-message"
+                                    />
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="btn btn-primary mb-4"
+                                >
+                                    Log in
+                                </button>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
             </div>
         </>
