054d3b9848d4c271d04861e284d55eb9e61421b3
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-03-15 23:33:10 +0530

    Update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
-import { LoginPage, NotFoundPage } from './pages'
+import { Route, Routes } from 'react-router-dom';
+import { HomePage, LoginPage, NotFoundPage } from './pages';
 
 function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" element={<HomePage />} />
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </>
-    )
+    );
 }
 
-export default App
+export default App;
