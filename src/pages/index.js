e88ba732d3d56cf6c5f376e68129ba95c07fb1e6
nirmalcodes <srnfernando102@gmail.com>
2023-04-04 01:20:27 +0530

    Add admin page for pages index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as LoginPage } from './LoginPage/LoginPage';
 export { default as HomePage } from './HomePage/HomePage';
+export { default as AdminPage } from './AdminPage/AdminPage';
 export { default as NotFoundPage } from './NotFoundPage/NotFoundPage';
