48edb63ee4f790ca7013dfa1ad4a70b9bc179830
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-24 19:05:17 +0530

    Add Routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
+import { Route, Routes } from 'react-router'
+import { HomePage, SignInPage, SignUpPage } from './pages'
 
 const App = () => {
     return (
@@ -7,7 +9,15 @@ const App = () => {
             <div className="relative flex min-h-screen flex-col overflow-hidden bg-[#f1f1f1]">
                 <Suspense>
                     <ErrorBoundary fallback={<div>Something went wrong</div>}>
-                        App Update
+                        <Routes>
+                            <Route path={'/'} element={<SignUpPage />} />
+                            <Route path={'/home'} element={<HomePage />} />
+                            <Route path={'/login'} element={<SignInPage />} />
+                            <Route
+                                path={'*'}
+                                element={<div>404 Page Not Found</div>}
+                            />
+                        </Routes>
                     </ErrorBoundary>
                 </Suspense>
             </div>
