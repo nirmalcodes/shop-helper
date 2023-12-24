d1777351b2ba5e2c0ce1c7122c3ffdc484e0e9a9
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-24 17:25:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const App = () => {
-  return (
-    <>
-    <div>App</div>
-    </>
-  )
+    return (
+        <>
+            <div className="relative flex min-h-screen flex-col overflow-hidden bg-[#f1f1f1]">
+                <Suspense>
+                    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                        App Update
+                    </ErrorBoundary>
+                </Suspense>
+            </div>
+        </>
+    )
 }
 
 export default App
