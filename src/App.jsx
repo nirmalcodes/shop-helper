daf5455b72c3387de176173621604c9c7e3a8a30
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-25 02:32:54 +0530

    Add AuthProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,38 @@ import React, { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Route, Routes } from 'react-router'
 import { HomePage, SignInPage, SignUpPage } from './pages'
+import { AuthProvider } from './contexts/AuthContext'
+import { ProtectedRoute } from './components'
 
 const App = () => {
     return (
         <>
             <div className="relative flex min-h-screen flex-col overflow-hidden bg-[#f1f1f1]">
                 <Suspense>
-                    <ErrorBoundary fallback={<div>Something went wrong...</div>}>
-                        <Routes>
-                            <Route path={'/'} element={<SignUpPage />} />
-                            <Route path={'/home'} element={<HomePage />} />
-                            <Route path={'/login'} element={<SignInPage />} />
-                            <Route
-                                path={'*'}
-                                element={<div>404 Page Not Found</div>}
-                            />
-                        </Routes>
+                    <ErrorBoundary
+                        fallback={<div>Something went wrong...</div>}
+                    >
+                        <AuthProvider>
+                            <Routes>
+                                <Route path={'/'} element={<SignUpPage />} />
+                                <Route
+                                    path={'/home'}
+                                    element={
+                                        <ProtectedRoute>
+                                            <HomePage />
+                                        </ProtectedRoute>
+                                    }
+                                />
+                                <Route
+                                    path={'/login'}
+                                    element={<SignInPage />}
+                                />
+                                <Route
+                                    path={'*'}
+                                    element={<div>404 Page Not Found</div>}
+                                />
+                            </Routes>
+                        </AuthProvider>
                     </ErrorBoundary>
                 </Suspense>
             </div>
