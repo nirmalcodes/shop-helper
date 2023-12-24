2d1110b2837059bf6c145e90467506b593faf6e9
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-25 02:32:38 +0530

    Update home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 const HomePage = () => {
+    const { user, logOut } = useContext(AuthContext)
+
+    const handleLogOut = async () => {
+        await logOut()
+    }
     return (
         <>
             <div>HomePage</div>
+            <p className="">Hello {user?.email}</p>
+            <button type="button" onClick={handleLogOut}>
+                log out
+            </button>
         </>
     )
 }
