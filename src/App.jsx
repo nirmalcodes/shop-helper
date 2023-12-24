6a018a89d87e0d2a2d7007848e4b69f6a8af5ac7
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-24 20:11:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         <>
             <div className="relative flex min-h-screen flex-col overflow-hidden bg-[#f1f1f1]">
                 <Suspense>
-                    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+                    <ErrorBoundary fallback={<div>Something went wrong...</div>}>
                         <Routes>
                             <Route path={'/'} element={<SignUpPage />} />
                             <Route path={'/home'} element={<HomePage />} />
