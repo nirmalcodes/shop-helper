a447e991ca9537de595ebced060ecd147fd2e61f
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-26 04:47:57 +0530

    Update components index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as ProtectedRoute } from './ProtectedRoute/ProtectedRoute'
+export { default as Layout } from './Layout/Layout'
 export { default as NumberInput } from './NumberInput/NumberInput'
