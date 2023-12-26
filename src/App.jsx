1b5af5511321217937f03ee4cc0f31dc250974b4
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-27 01:19:51 +0530

    Add RoutesProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ErrorBoundary } from 'react-error-boundary'
 import { Route, Routes } from 'react-router'
 import { HomePage, SignInPage, SignUpPage } from './pages'
 import { AuthProvider } from './contexts/AuthContext'
+import { RoutesProvider } from './contexts/RoutesContext'
 import { ProtectedRoute } from './components'
 
 const App = () => {
@@ -14,46 +15,48 @@ const App = () => {
                         fallback={<div>Something went wrong...</div>}
                     >
                         <AuthProvider>
-                            <Routes>
-                                <Route
-                                    path={'/signin'}
-                                    element={<SignInPage />}
-                                />
-                                <Route
-                                    path={'/signup'}
-                                    element={<SignUpPage />}
-                                />
+                            <RoutesProvider>
+                                <Routes>
+                                    <Route
+                                        path={'/signin'}
+                                        element={<SignInPage />}
+                                    />
+                                    <Route
+                                        path={'/signup'}
+                                        element={<SignUpPage />}
+                                    />
 
-                                <Route
-                                    path={'/'}
-                                    element={
-                                        <ProtectedRoute>
-                                            <HomePage />
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route
-                                    path={'/koko'}
-                                    element={
-                                        <ProtectedRoute>
-                                            <HomePage />
-                                        </ProtectedRoute>
-                                    }
-                                />
-                                <Route
-                                    path={'/updates'}
-                                    element={
-                                        <ProtectedRoute>
-                                            <HomePage />
-                                        </ProtectedRoute>
-                                    }
-                                />
+                                    <Route
+                                        path={'/'}
+                                        element={
+                                            <ProtectedRoute>
+                                                <HomePage />
+                                            </ProtectedRoute>
+                                        }
+                                    />
+                                    <Route
+                                        path={'/koko'}
+                                        element={
+                                            <ProtectedRoute>
+                                                <HomePage />
+                                            </ProtectedRoute>
+                                        }
+                                    />
+                                    <Route
+                                        path={'/updates'}
+                                        element={
+                                            <ProtectedRoute>
+                                                <HomePage />
+                                            </ProtectedRoute>
+                                        }
+                                    />
 
-                                <Route
-                                    path={'*'}
-                                    element={<div>404 Page Not Found</div>}
-                                />
-                            </Routes>
+                                    <Route
+                                        path={'*'}
+                                        element={<div>404 Page Not Found</div>}
+                                    />
+                                </Routes>
+                            </RoutesProvider>
                         </AuthProvider>
                     </ErrorBoundary>
                 </Suspense>
