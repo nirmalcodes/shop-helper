72d3a730219813e09fd85303f51eb729271302c3
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-31 03:55:31 +0530

    Update pages index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,10 @@
-export { default as HomePage } from './HomePage/HomePage'
 export { default as SignInPage } from './SignInPage/SignInPage'
 export { default as SignUpPage } from './SignUpPage/SignUpPage'
+
+export { default as HomePage } from './HomePage/HomePage'
+
+export { default as KOKOPage } from './KOKOPage/KOKOPage'
+export { default as KOKOSettingsPage } from './KOKOSettingsPage/KOKOSettingsPage'
+
+export { default as UpdatesPage } from './UpdatesPage/UpdatesPage'
+export { default as UpdatesSettingsPage } from './UpdatesSettingsPage/UpdatesSettingsPage'
