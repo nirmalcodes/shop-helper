655581b5c04d4461edf7118051679a1a08cc48f0
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-31 03:54:52 +0530

    Add KOKOSettingsPage to pages

---
+++ src/pages/KOKOSettingsPage/KOKOSettingsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const KOKOSettingsPage = () => {
+    return (
+        <>
+            <div>KOKOSettingsPage</div>
+        </>
+    )
+}
+
+export default KOKOSettingsPage
