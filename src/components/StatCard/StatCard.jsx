2e21c0d0be8a2e82a6a86217ccfec870d42f28cb
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2023-12-31 20:15:23 +0530

    Update StatCard component

--- src/components/StatCard/StatCard.jsx
+++ src/components/StatCard/StatCard.jsx
@@ -6,12 +6,14 @@ const StatCard = ({ data }) => {
             <div className="flex min-h-[100px] w-full max-w-[350px] items-center gap-x-3 rounded-lg border-l-4 border-[#021EA3] bg-white p-4 shadow-md">
                 <div className="flex-1">
                     <h5 className="mb-1 text-sm font-bold text-[#021EA3]">
-                        {data?.name}
+                        {data?.name ? data?.name : '-'}
                     </h5>
-                    <p className="text-xl font-medium">{data?.value}</p>
+                    <p className="text-xl font-medium text-slate-700">
+                        {data?.value ? data?.value : '-'}
+                    </p>
                 </div>
                 {data?.icon && (
-                    <div className="grid h-[62px] w-[62px] shrink-0 place-items-center p-1 text-4xl text-slate-500/50">
+                    <div className="grid h-[62px] w-[62px] shrink-0 place-items-center p-1 text-4xl text-slate-500/35">
                         <data.icon />
                     </div>
                 )}
