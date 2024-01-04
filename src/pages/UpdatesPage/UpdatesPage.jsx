c9f14d02db303da18a2f5a438405aa8e9c8ffbf0
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-01-04 16:28:38 +0530

    Update updates page

--- src/pages/UpdatesPage/UpdatesPage.jsx
+++ src/pages/UpdatesPage/UpdatesPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { UpdatesCard } from '../../components'
+import { FaPlus } from 'react-icons/fa6'
 
 const UpdatesPage = () => {
     return (
         <>
-            <div className="container">
-                <div className="flex flex-wrap gap-y-4">
-                    <div className="w-full lg:w-7/12 lg:px-4">
-                        <div className="h-[calc(100vh_-_230px)] max-h-[calc(100vh_-_230px)] overflow-hidden overflow-y-auto rounded-lg bg-white px-4 py-5 shadow-md">
-                            news
+            <div className="container bg-green-500">
+                <div className="relative flex flex-wrap gap-y-4">
+                    <div className="w-full bg-red-500 lg:w-7/12 lg:px-4">
+                        <div className="">
+                            <UpdatesCard data={''} />
+                            <UpdatesCard data={''} />
+                            <UpdatesCard data={''} />
+                            <UpdatesCard data={''} />
                             <UpdatesCard data={''} />
                             <UpdatesCard data={''} />
                             <UpdatesCard data={''} />
@@ -23,6 +27,12 @@ const UpdatesPage = () => {
                     </div>
                 </div>
             </div>
+            <button
+                type="button"
+                className="fixed bottom-4 right-4 flex h-[56px] w-[56px] items-center justify-center rounded-full border border-transparent bg-indigo-600 p-4 font-medium text-white shadow-sm hover:bg-indigo-700  focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 disabled:bg-indigo-700 md:bottom-5"
+            >
+                <FaPlus className="text-4xl" />
+            </button>
         </>
     )
 }
