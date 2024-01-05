02c72ddfd621f55b22e681275c2f49f1ee5dc20f
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-01-06 01:30:38 +0530

    Update modal component

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 
-const Modal = () => {
-    const [isOpen, setIsOpen] = useState(false)
+const Modal = ({ title = 'Title', children }) => {
+    const [isOpen, setIsOpen] = useState(true)
 
     const closeModal = () => {
         setIsOpen(false)
@@ -44,19 +44,15 @@ const Modal = () => {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                                    <Dialog.Title
-                                        as="h3"
-                                        className="text-lg font-medium leading-6 text-gray-900"
-                                    >
-                                        Payment successful
-                                    </Dialog.Title>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Your payment has been successfully
-                                            submitted. We’ve sent you an email
-                                            with all of the details of your
-                                            order.
-                                        </p>
+                                    {title && (
+                                        <Dialog.Title
+                                            as="h3"
+                                            className="text-lg font-medium leading-6 text-gray-900"
+                                        >
+                                            {title}
+                                        </Dialog.Title>
+                                    )}
+                                    <div className="mt-3">
                                         <form className="">
                                             <div className="form-group">
                                                 <textarea
