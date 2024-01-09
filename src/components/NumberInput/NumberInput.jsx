2782307d849ac2566581b66af77b716a21f5a988
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-01-09 22:23:04 +0530

    Update NumberInput component

--- src/components/NumberInput/NumberInput.jsx
+++ src/components/NumberInput/NumberInput.jsx
@@ -10,10 +10,18 @@ const NumberInput = ({ ...rest }) => {
             event.preventDefault()
         }
 
+        const handleKeyDown = (event) => {
+            if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+                event.preventDefault()
+            }
+        }
+
         input.addEventListener('wheel', handleWheel, { passive: false })
+        input.addEventListener('keydown', handleKeyDown)
 
         return () => {
             input.removeEventListener('wheel', handleWheel)
+            input.removeEventListener('keydown', handleKeyDown)
         }
     }, [])
 
