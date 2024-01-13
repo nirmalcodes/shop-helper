e65d7ff2ca23e7cb655f29e4839f585f460ac9e6
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-01-13 06:44:12 +0530

    Update settings page

--- src/pages/SettingsPage/SettingsPage.jsx
+++ src/pages/SettingsPage/SettingsPage.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 
 const SettingsPage = () => {
     const tabs = [
-        { id: 1, name: 'User Settings' },
-        { id: 2, name: 'KOKO Settings' },
-        { id: 3, name: 'Updates Settings' },
+        { id: 1, name: 'User Settings', path: '' },
+        { id: 2, name: 'KOKO Settings', path: '/settings/koko_settings' },
+        { id: 3, name: 'Updates Settings', path: '' },
     ]
 
     return (
         <div className="container grid grid-cols-1 gap-3 px-4 py-5">
             {tabs.map((tab) => (
-                <Link key={tab?.id}>
+                <Link key={tab?.id} to={tab?.path}>
                     <div className="flex items-center rounded-md border bg-white p-4 shadow-md">
-                        <h5 className="text-lg font-medium">{tab?.name}</h5>
+                        <h5 className="text-lg">{tab?.name}</h5>
                         <FaChevronRight className="ml-auto text-lg" />
                     </div>
                 </Link>
