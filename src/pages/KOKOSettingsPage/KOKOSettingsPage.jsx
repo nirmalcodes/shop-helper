e6461ab8f886996cf8cb71529b73fa9589052c56
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-01-13 06:44:23 +0530

    Update koko settings page

--- src/pages/KOKOSettingsPage/KOKOSettingsPage.jsx
+++ src/pages/KOKOSettingsPage/KOKOSettingsPage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Breadcrumbs } from '../../components'
 
 const KOKOSettingsPage = () => {
     return (
         <>
+            <Breadcrumbs />
             <div>KOKOSettingsPage</div>
         </>
     )
