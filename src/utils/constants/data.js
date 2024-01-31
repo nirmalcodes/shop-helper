6e7b46e0feced676180983423d9b6fc9793b14d6
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-01 02:14:06 +0530

    Update data constant

--- src/utils/constants/data.js
+++ src/utils/constants/data.js
@@ -1,6 +1,6 @@
 const roles = [
     { name: 'Admin', value: 'admin' },
-    { name: 'Moderator', value: 'moderator' },
+    // { name: 'Moderator', value: 'moderator' },
     { name: 'User', value: 'user' },
 ]
 
