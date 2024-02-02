bfee0824d8f766d62c1c74a564aecb9b596fe061
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-03 01:52:15 +0530

    Update sign in page sign in functions and validations

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,6 +3,8 @@ import { AuthContext } from '../../contexts/AuthContext'
 import { Navigate, useLocation } from 'react-router-dom'
 import { FaEye, FaEyeSlash } from 'react-icons/fa6'
 import { validateForm } from '../../utils/helpers/validators/SignInUpFormValidator'
+import { signInWithEmailAndPassword } from '@firebase/auth'
+import { auth } from '../../services/firebase/firebase'
 
 const SignIn = () => {
     const { user, emailSignIn } = useContext(AuthContext)
@@ -12,34 +14,96 @@ const SignIn = () => {
         return <Navigate to={'/'} replace />
     }
 
-    // if (user) {
-    //     return <Navigate to={'/'} replace />
-    // }
+    const [isLoading, setIsLoading] = useState(false)
+    const [isToggled, setIstoggled] = useState(false)
 
     const [formData, setFormData] = useState({
         email: '',
         password: '',
     })
-    const [isToggled, setIstoggled] = useState(false)
-    const [errors, setErrors] = useState({})
+    const [errors, setErrors] = useState({
+        email: '',
+        password: '',
+        common: '',
+    })
 
-    const handleChange = (event) => {
-        setFormData({ ...formData, [event.target.name]: event.target.value })
-        setErrors({})
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value })
+        setErrors({ ...errors, [name]: '' })
     }
 
     const togglePassword = () => {
         setIstoggled((prev) => !prev)
     }
 
+    const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        return emailRegex.test(email)
+    }
+
+    const isValidPassword = (password) => {
+        const passwordRegex =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+        return passwordRegex.test(password)
+    }
+
     const handleSignIn = async (e) => {
         e.preventDefault()
+        setIsLoading(true)
+        setErrors({})
 
-        const validationErrors = validateForm(formData)
-        setErrors(validationErrors)
+        const validationErrors = {}
 
-        if (Object.keys(validationErrors).length === 0) {
-            await emailSignIn(formData.email, formData.password)
+        if (!formData.email.trim()) {
+            validationErrors.email = 'Email is required'
+        } else if (!isValidEmail(formData.email)) {
+            validationErrors.email = 'Invalid email format'
+        }
+
+        if (!formData.password.trim()) {
+            validationErrors.password = 'Password is required'
+        } else if (!isValidPassword(formData.password)) {
+            validationErrors.password = 'Password must be at least 8 characters'
+        }
+
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors)
+            setIsLoading(false)
+        } else {
+            try {
+                await signInWithEmailAndPassword(
+                    auth,
+                    formData.email,
+                    formData.password
+                )
+                setFormData({})
+            } catch (error) {
+                const errorCode = error.code
+
+                if (errorCode === 'auth/invalid-email') {
+                    setErrors((prevErrors) => ({
+                        ...prevErrors,
+                        email: 'Invalid email address',
+                    }))
+                } else if (errorCode === 'auth/user-not-found') {
+                    setErrors((prevErrors) => ({
+                        ...prevErrors,
+                        email: 'This email is not registered',
+                    }))
+                } else if (errorCode === 'auth/wrong-password') {
+                    setErrors((prevErrors) => ({
+                        ...prevErrors,
+                        password: 'Incorrect password',
+                    }))
+                } else {
+                    setErrors((prevErrors) => ({
+                        ...prevErrors,
+                        common: 'An error occurred. Please try again later',
+                    }))
+                }
+            }
+            setIsLoading(false)
         }
     }
 
@@ -70,7 +134,7 @@ const SignIn = () => {
                         name="email"
                         id="eamil"
                         value={formData.email}
-                        onChange={handleChange}
+                        onChange={handleInputChange}
                         required
                         className={
                             'w-full appearance-none rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 disabled:bg-gray-300/30 disabled:text-gray-600'
@@ -97,7 +161,7 @@ const SignIn = () => {
                             name="password"
                             id="password"
                             value={formData.password}
-                            onChange={handleChange}
+                            onChange={handleInputChange}
                             required
                             className={
                                 'w-full appearance-none rounded-lg border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 disabled:bg-gray-300/30 disabled:text-gray-600'
@@ -119,13 +183,18 @@ const SignIn = () => {
                         </span>
                     )}
                 </div>
+                {errors.common && (
+                    <span className="text-sm text-red-600">
+                        {errors.common}
+                    </span>
+                )}
                 <button
                     type="submit"
                     className={
                         'flex w-full justify-center rounded-lg border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium  text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:bg-indigo-700'
                     }
                 >
-                    Log In
+                    {isLoading ? 'Logging in...' : 'Log in'}
                 </button>
             </form>
         </div>
