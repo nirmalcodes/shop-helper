919d63c5e171e48f47442d4375d50b3903105007
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-03 15:09:02 +0530

    Update update card component

--- src/components/UpdateCard/UpdateCard.jsx
+++ src/components/UpdateCard/UpdateCard.jsx
@@ -1,29 +1,43 @@
-import React, { useEffect, useRef } from 'react'
-import { auth } from '../../services/firebase/firebase'
+import React, { useEffect, useRef, useState } from 'react'
+import { auth, firestore } from '../../services/firebase/firebase'
+import { doc, getDoc } from '@firebase/firestore'
+import { formatDateTime } from '../../utils/helpers/formatters/formatDateTime'
 
 const UpdateCard = ({ update }) => {
+    const { createdBy, message, createdAt } = update
     const UpdateRef = useRef(null)
+    const [docData, setDocData] = useState({})
+
+    const fetchUsername = async (userId) => {
+        const userDocRef = doc(firestore, 'users', userId)
+        const docSnap = await getDoc(userDocRef)
+        if (docSnap.exists()) {
+            setDocData({ ...docSnap.data() })
+        }
+    }
 
     useEffect(() => {
+        fetchUsername(createdBy)
         UpdateRef.current?.scrollIntoView({ behavior: 'smooth' })
     }, [update])
 
     return (
         <div className="flex items-end bg-yellow-300/0" ref={UpdateRef}>
             <div
-                className={`mb-2 flex w-fit max-w-[80%] flex-col rounded-md p-2 shadow-md lg:max-w-[55%]${
+                className={`mb-2 flex w-fit max-w-[80%] flex-col rounded-md p-2 shadow lg:max-w-[55%]${
                     auth.currentUser.uid === update?.createdBy
-                        ? ' ml-auto bg-green-200'
-                        : ' bg-white'
+                        ? ' ml-auto rounded-br-none bg-green-200'
+                        : ' rounded-tl-none bg-white'
                 }`}
             >
-                <p className="mb-[2px] text-xs font-medium">
-                    {update?.createdBy}
+                <p className="mb-[2px] text-xs font-medium text-gray-500">
+                    {docData.username ?? '-'}
                 </p>
-                <p className="mb-[2px] text-sm">{update?.message}</p>
-                <span className="ml-auto inline-block text-[0.6875rem]">
-                    {update?.createdAt?.seconds}
-                </span>
+                <p className="text-sm">{message}</p>
+                {/* <p className="mb-[2px] text-sm">{message}</p> */}
+                {/* <span className="ml-auto inline-block text-[0.6875rem]">
+                    {formatDateTime(createdAt?.seconds) ?? '-'}
+                </span> */}
             </div>
         </div>
     )
