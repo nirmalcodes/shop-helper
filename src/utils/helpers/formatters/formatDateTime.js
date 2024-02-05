90a8929a47778f0286dc562b937c65a7a7a612be
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-06 05:20:56 +0530

    Update date format helper

--- src/utils/helpers/formatters/formatDateTime.js
+++ src/utils/helpers/formatters/formatDateTime.js
@@ -1,20 +1,21 @@
 import { format } from 'date-fns'
 
 export const formatDateTime = (
-    timestampInSeconds,
-    formatString = 'dd/MM/yyyy HH:mm:ss aa'
+    timestamp,
+    formatString = 'dd/MM/yyyy hh:mm:ss aa'
 ) => {
-    // Sample timestamp in seconds
-    // const timestampInSeconds = 1705182377
+    let formattedDate
+    if (isNaN(timestamp)) {
+        formattedDate = null
+    } else {
+        // Convert timestamp to a Date object
+        const date = new Date(timestamp * 1000)
 
-    // Convert timestamp to a Date object
-    const date = new Date(timestampInSeconds * 1000)
+        // Format the date using date-fns
+        formattedDate = format(date, formatString)
 
-    // Format the date using date-fns
-    // const formattedDate = format(date, 'dd/MM/yyyy HH:mm:ss aa')
-    const formattedDate = format(date, formatString)
-
-    // console.log(formattedDate)
+        // console.log(formattedDate)
+    }
 
     return formattedDate
 }
