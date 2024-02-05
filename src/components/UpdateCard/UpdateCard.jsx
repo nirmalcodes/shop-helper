b2db58188adb1c509376403a5f929dc6d9f461da
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-06 05:18:57 +0530

    Update update card component

--- src/components/UpdateCard/UpdateCard.jsx
+++ src/components/UpdateCard/UpdateCard.jsx
@@ -2,9 +2,56 @@ import React, { useEffect, useRef, useState } from 'react'
 import { auth, firestore } from '../../services/firebase/firebase'
 import { doc, getDoc } from '@firebase/firestore'
 import { formatDateTime } from '../../utils/helpers/formatters/formatDateTime'
+import { images } from '../../utils/constants'
+import Thumbnails from '../Thumbnails/Thumbnails'
+
+export const Thumbnailer = ({ dataArray }) => {
+    let renderedItems
+
+    if (dataArray.length <= 4) {
+        renderedItems = dataArray.map((item, index) => (
+            <img
+                key={item.id}
+                title={index}
+                src={item.url}
+                alt={item.name}
+                loading="lazy"
+                width={170}
+                className="h-[168px] w-[168px] rounded object-cover"
+            />
+        ))
+    } else {
+        renderedItems = dataArray
+            .slice(0, 3)
+            .map((item, index) => (
+                <img
+                    key={item.id}
+                    title={index}
+                    src={item.url}
+                    alt={item.name}
+                    loading="lazy"
+                    width={170}
+                    className="h-[168px] w-[168px] rounded object-cover"
+                />
+            ))
+        const remainingItems = dataArray.length - 3
+        const buttonText = `Show ${remainingItems} more`
+
+        const handleShowMore = () => {
+            console.log('Show more button clicked')
+        }
+
+        renderedItems.push(
+            <button key="showMoreButton" onClick={handleShowMore}>
+                {buttonText}
+            </button>
+        )
+    }
+    return renderedItems
+}
 
 const UpdateCard = ({ update }) => {
-    const { createdBy, message, createdAt } = update
+    const { createdBy, message } = update
     const UpdateRef = useRef(null)
     const [docData, setDocData] = useState({})
 
@@ -33,11 +80,15 @@ const UpdateCard = ({ update }) => {
                 <p className="mb-[2px] text-xs font-medium text-gray-500">
                     {docData.username ?? '-'}
                 </p>
-                <p className="text-sm">{message}</p>
+                <p className="text-sm">{update?.message ?? '-'}</p>
+
+                <div className="flex max-w-max flex-wrap gap-1">
+                    <Thumbnails dataArray={filesData} />
+                </div>
                 {/* <p className="mb-[2px] text-sm">{message}</p> */}
-                {/* <span className="ml-auto inline-block text-[0.6875rem]">
-                    {formatDateTime(createdAt?.seconds) ?? '-'}
-                </span> */}
+                <span className="ml-auto inline-block text-[0.6875rem]">
+                    {formatDateTime(update?.createdAt?.seconds) ?? '-'}
+                </span>
             </div>
         </div>
     )
