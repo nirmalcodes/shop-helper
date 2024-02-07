2a0cccbd8ff88a6605c187da57e49880799fa5a7
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-02-07 06:32:17 +0530

    Update message card component

--- src/components/MessageCard/MessageCard.jsx
+++ src/components/MessageCard/MessageCard.jsx
@@ -133,73 +133,7 @@ const MessageCard = ({
                                 <div className="name text-sm font-medium">
                                     {username ?? '-'}
                                 </div>
-                                <div className="ml-auto leading-none">
-                                    {/* dropdown */}
-                                    <Menu
-                                        as="div"
-                                        className="relative inline-block text-left"
-                                    >
-                                        <div>
-                                            <Menu.Button className="inline-flex justify-center text-sm focus:outline-none focus-visible:ring-0 focus-visible:ring-transparent">
-                                                <FaEllipsisVertical />
-                                            </Menu.Button>
-                                        </div>
-                                        <Transition
-                                            as={Fragment}
-                                            enter="transition ease-out duration-100"
-                                            enterFrom="transform opacity-0 scale-95"
-                                            enterTo="transform opacity-100 scale-100"
-                                            leave="transition ease-in duration-75"
-                                            leaveFrom="transform opacity-100 scale-100"
-                                            leaveTo="transform opacity-0 scale-95"
-                                        >
-                                            <Menu.Items className="absolute right-0 mt-2 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
-                                                <div className="px-1 py-1">
-                                                    <Menu.Item>
-                                                        {({ active }) => (
-                                                            <button
-                                                                className={`font-medium ${
-                                                                    active
-                                                                        ? ' bg-black/5 text-gray-900'
-                                                                        : ' text-gray-900'
-                                                                } group flex w-full items-center rounded-md px-2 py-2`}
-                                                                onClick={(
-                                                                    e
-                                                                ) => {
-                                                                    openDltModal(
-                                                                        messageId
-                                                                    )
-                                                                }}
-                                                            >
-                                                                <FaRegTrashCan className="mr-1" />
-                                                                Delete
-                                                            </button>
-                                                        )}
-                                                    </Menu.Item>
-                                                </div>
-                                            </Menu.Items>
-                                        </Transition>
-                                    </Menu>
-                                </div>
-                            </div>
-                            <p className="message mr-6">{message ?? '-'}</p>
-                            <span className="date-time ml-6 block text-right text-xs">
-                                {formatDateTime(timestamp) ?? '-'}
-                            </span>
-                        </div>
-                    )}
-                    {/* attachments */}
-                    {attachments.length > 0 && (
-                        <div
-                            className={`mssg-attachment-card${
-                                own ? ' bg-green-200' : ' bg-white'
-                            }`}
-                        >
-                            {message.trim().length === 0 && (
-                                <div className="flex px-1">
-                                    <div className="name pb-[2px] text-sm font-medium">
-                                        {username ?? '-'}
-                                    </div>
+                                {own && (
                                     <div className="ml-auto leading-none">
                                         {/* dropdown */}
                                         <Menu
@@ -248,6 +182,78 @@ const MessageCard = ({
                                             </Transition>
                                         </Menu>
                                     </div>
+                                )}
+                            </div>
+                            <p className="message mr-6">{message ?? '-'}</p>
+                            <span className="date-time ml-6 block text-right text-xs">
+                                {formatDateTime(timestamp) ?? '-'}
+                            </span>
+                        </div>
+                    )}
+                    {/* attachments */}
+                    {attachments.length > 0 && (
+                        <div
+                            className={`mssg-attachment-card${
+                                own ? ' bg-green-200' : ' bg-white'
+                            }`}
+                        >
+                            {message.trim().length === 0 && (
+                                <div className="flex px-1">
+                                    <div className="name pb-[2px] text-sm font-medium">
+                                        {username ?? '-'}
+                                    </div>
+                                    {own && (
+                                        <div className="ml-auto leading-none">
+                                            {/* dropdown */}
+                                            <Menu
+                                                as="div"
+                                                className="relative inline-block text-left"
+                                            >
+                                                <div>
+                                                    <Menu.Button className="inline-flex justify-center text-sm focus:outline-none focus-visible:ring-0 focus-visible:ring-transparent">
+                                                        <FaEllipsisVertical />
+                                                    </Menu.Button>
+                                                </div>
+                                                <Transition
+                                                    as={Fragment}
+                                                    enter="transition ease-out duration-100"
+                                                    enterFrom="transform opacity-0 scale-95"
+                                                    enterTo="transform opacity-100 scale-100"
+                                                    leave="transition ease-in duration-75"
+                                                    leaveFrom="transform opacity-100 scale-100"
+                                                    leaveTo="transform opacity-0 scale-95"
+                                                >
+                                                    <Menu.Items className="absolute right-0 mt-2 w-52 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+                                                        <div className="px-1 py-1">
+                                                            <Menu.Item>
+                                                                {({
+                                                                    active,
+                                                                }) => (
+                                                                    <button
+                                                                        className={`font-medium ${
+                                                                            active
+                                                                                ? ' bg-black/5 text-gray-900'
+                                                                                : ' text-gray-900'
+                                                                        } group flex w-full items-center rounded-md px-2 py-2`}
+                                                                        onClick={(
+                                                                            e
+                                                                        ) => {
+                                                                            openDltModal(
+                                                                                messageId
+                                                                            )
+                                                                        }}
+                                                                    >
+                                                                        <FaRegTrashCan className="mr-1" />
+                                                                        Delete
+                                                                    </button>
+                                                                )}
+                                                            </Menu.Item>
+                                                        </div>
+                                                    </Menu.Items>
+                                                </Transition>
+                                            </Menu>
+                                        </div>
+                                    )}
                                 </div>
                             )}
                             <div className="attachment-wrapper">
