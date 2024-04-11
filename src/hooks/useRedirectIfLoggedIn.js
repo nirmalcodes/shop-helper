dc4da00ce19f14d5ece3bd0e23751e28bffd9aec
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-04-11 06:24:07 +0530

    Update user redirect hook

--- src/hooks/useRedirectIfLoggedIn.js
+++ src/hooks/useRedirectIfLoggedIn.js
@@ -1,19 +1,29 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import ROUTES from '../routes';
 import { AuthContext } from '../contexts/AuthContext';
-import { Navigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const useRedirectIfLoggedIn = () => {
     const { user } = useContext(AuthContext);
     const location = useLocation();
+    const navigate = useNavigate();
+    const redirectPathRef = useRef(null);
 
-    const redirectPath = ROUTES.find(
-        (route) => route.main && route.protected && !route.hiddenInSidebar
-    )?.path;
-
-    if (user && location.pathname !== redirectPath) {
-        return <Navigate to={redirectPath} replace />;
+    if (!redirectPathRef.current) {
+        redirectPathRef.current = ROUTES.find(
+            (route) => route.main && route.protected && !route.hiddenInSidebar
+        )?.path;
     }
+
+    useEffect(() => {
+        const redirectPath = redirectPathRef.current;
+
+        if (user && location.pathname !== redirectPath) {
+            navigate(redirectPath);
+        }
+
+        return () => {};
+    }, [user, location.pathname]);
 
     return null;
 };
