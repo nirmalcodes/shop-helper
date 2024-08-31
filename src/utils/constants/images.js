16de4d62193f261689e63cc6c0ce8e975696cb7e
nirmalcodes <119014888+nirmalcodes@users.noreply.github.com>
2024-08-31 20:54:38 +0530

    Update images constant

--- src/utils/constants/images.js
+++ src/utils/constants/images.js
@@ -1,5 +1,5 @@
-import backgroundImg from './background_shapes.jpg'
-import kokoLogo from './koko_logo.png'
+import backgroundImg from '/background_shapes.jpg'
+import kokoLogo from '/koko_logo.png'
 
 export default {
     backgroundImg,
